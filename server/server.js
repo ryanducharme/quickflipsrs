09b5005f395827c96c3822d8f81a946ec4367081
Ryan DuCharme <ryan.ducharme77@gmail.com>
2022-09-13 20:51:22 -0400

    update

--- server/server.js
+++ server/server.js
@@ -1,52 +1,61 @@
 const express = require("express");
-const MongoClient = require("mongodb");
+const MongoClient = require("mongodb").MongoClient;
 const bcrypt = require('bcrypt');
+const dotenv = require('dotenv').config({ path: __dirname + '/.env' });
 const app = express();
+const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3001;
-
-
-const connectionString = process.env;
-console.log(connectionString);
+const connectionString = process.env.ATLAS_URI;
+const { connectToDb, getDb } = require('./db')
 const users = [];
-
 app.set('view-engine', 'ejs');
 app.use(express.urlencoded({ extended: false }))
 
-app.get('/', (req, res) => {
-  res.render("index.ejs")
-})
+let db;
 
-app.get("/register", (req, res) => {
-  res.render('register.ejs');
-});
+connectToDb((err) => {
+  if (!err) {
+    console.log(`Connected to MongoDB Atlas`);
+    app.listen(PORT, () => {
+      console.log(`Server listening on ${PORT}`);
+    });
 
-app.post('/register', async (req, res) => {
-  try {
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    users.push(
-      {
-        id: Date.now().toString(),
-        name: req.body.name,
-        email: req.body.email,
-        password: hashedPassword
-      })
-    res.redirect('/login')
-  } catch {
-    res.redirect('/register')
+    db = getDb();
+
+    app.get('/', (req, res) => {
+      res.render("index.ejs")
+    })
+
+    app.get("/register", (req, res) => {
+      res.render('register.ejs');
+
+    });
+
+    app.post('/register', async (req, res) => {
+      try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        db.collection('users').insertOne(
+          {
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+          })
+        res.redirect('/login')
+      } catch {
+        res.redirect('/register')
+      }
+      console.log(users);
+
+    })
+
+    app.get("/login", (req, res) => {
+      res.render('login.ejs');
+    });
+
+    app.post('/login', (req, res) => {
+
+    })
+
   }
-  console.log(users);
-
 })
 
-
-app.get("/login", (req, res) => {
-  res.render('login.ejs');
-});
-
-app.post('/login', (req, res) => {
-
-})
-
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
