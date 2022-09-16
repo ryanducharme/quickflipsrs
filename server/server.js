740751adbf8fba96d8f2d27544aee60d7f83c16f
Ryan <72882588+ryanducharme@users.noreply.github.com>
2022-09-15 22:44:46 -0400

    added axios and basic api call

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const dotenv = require('dotenv').config({ path: __dirname + '/.env' });
 const path = require('path');
 const mongoose = require('mongoose');
 const chart = require('chart.js');
-
+const axios = require('axios');
 const app = express();
 const PORT = process.env.PORT || 3001;
 const connectionString = process.env.ATLAS_URI;
@@ -25,6 +25,7 @@ app.use('/', require('./routes/index'))
 app.use('/users', require('./routes/users'))
 app.use('/exchange', require('./routes/exchange'))
 
+let mapping = [];
 
 mongoose.connect(connectionString, { useNewUrlParser: true })
   .then(() => {
@@ -32,5 +33,15 @@ mongoose.connect(connectionString, { useNewUrlParser: true })
     app.listen(PORT, () => {
       console.log(`Server listening on ${PORT}`);
     });
+
+
+
+    axios.get('https://prices.runescape.wiki/api/v1/osrs/mapping')
+      .then(response => {
+        // console.log(response);
+        mapping = res;
+      })
+      .catch(error => {
+        console.log(error);
+      });
   })
-  .catch(err => console.log(err))
